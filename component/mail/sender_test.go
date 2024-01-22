740f60bc942a7e70effe8a225e5140708a9d5b9a
coungdev <coungdev@gmail.com>
2024-01-22 17:37:46 +0700

    test email sender

--- component/mail/sender_test.go
+++ component/mail/sender_test.go
@@ -1,7 +1,27 @@
 package mail
 
-import "testing"
+import (
+	"quizen/config"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestSendEmailWithGmail(t *testing.T) {
+	config, err := config.LoadConfig("../../")
+	require.NoError(t, err)
 
+	sender := NewGmailSender(config.EmailSenderName, config.EmailSenderAddress, config.EmailSenderPassword)
+
+	subject := "Test subject"
+	content := `
+	<h1>Test content</h1>
+	`
+	to := []string{"duongcongcuong02012004@gmail.com"}
+
+	attachFiles := []string{"../../README.md"}
+
+	err = sender.SendEmail(subject, content, to, nil, nil, attachFiles)
+
+	require.NoError(t, err)
 }

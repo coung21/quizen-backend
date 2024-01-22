981c16607623114b10345b1a9e0d7eb5cb780b95
coungdev <coungdev@gmail.com>
2024-01-23 00:39:26 +0700

    custom log ouptut

--- main.go
+++ main.go
@@ -2,16 +2,23 @@ package main
 
 import (
 	"log"
+	"os"
 	"quizen/component/worker"
 	"quizen/config"
 	"quizen/db"
 	"quizen/middleware"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/hibiken/asynq"
+	"github.com/rs/zerolog"
+	zlog "github.com/rs/zerolog/log"
 )
 
 func main() {
+	output := zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339}
+	zlog.Logger = zlog.Output(output)
+
 	r := gin.New()
 	r.Use(middleware.Logger(), gin.Recovery())
 
@@ -29,11 +36,11 @@ func main() {
 	_ = worker.NewRedisTaskDistributor(asynq.RedisClientOpt{Addr: config.RedisAddress})
 	go worker.RunTaskProcessor(asynq.RedisClientOpt{Addr: config.RedisAddress})
 
-	// r.GET("/ping", func(c *gin.Context) {
-	// 	c.JSON(200, gin.H{
-	// 		"message": "pong",
-	// 	})
-	// })
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
 
 	r.Run(config.ServerAddress)
 }

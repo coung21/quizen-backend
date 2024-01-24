e08ecf4380058af716bca52b12e82995273a62d5
coungdev <coungdev@gmail.com>
2024-01-24 08:36:47 +0700

    Add UserStore implementation

--- module/user/store/store.go
+++ module/user/store/store.go
@@ -1,1 +1,20 @@
 package store
+
+import (
+	"context"
+	"quizen/module/user/model"
+
+	"gorm.io/gorm"
+)
+
+type Store interface {
+	CreateUser(context.Context, *model.User) (*model.User, error)
+}
+
+type UserStore struct {
+	db *gorm.DB
+}
+
+func NewUserStore(db *gorm.DB) *UserStore {
+	return &UserStore{db: db}
+}
---
+++ module/user/store/user_store.go
@@ -0,0 +1,48 @@
+package store
+
+import (
+	"context"
+	"quizen/common"
+	"quizen/module/user/model"
+)
+
+type UpdateUserParam struct {
+	Password   *string
+	Avatar     *common.Image
+	IsVerified *bool
+}
+
+func (s *UserStore) CreateUser(ctx context.Context, user *model.User) (*model.User, error) {
+	if err := s.db.Create(user).Error; err != nil {
+		return nil, err
+	}
+	return user, nil
+}
+
+func (s *UserStore) GetUserByEmail(ctx context.Context, email string) (*model.User, error) {
+	var user model.User
+	if err := s.db.Where("email = ?", email).First(&user).Error; err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (s *UserStore) GetUserById(ctx context.Context, id int) (*model.User, error) {
+	var user model.User
+	if err := s.db.Where("id = ?", id).First(&user).Error; err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (s *UserStore) UpdateUser(ctx context.Context, id int, user *model.User) (*model.User, error) {
+	param := UpdateUserParam{
+		Password:   &user.Password,
+		Avatar:     user.Avatar,
+		IsVerified: &user.IsVerifed,
+	}
+	if err := s.db.Model(&user).Where("id = ?", id).Updates(param).Error; err != nil {
+		return nil, err
+	}
+	return user, nil
+}

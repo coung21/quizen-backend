66ca3e7d85a8bd392c767a7956fe6253bd0a2867
coungdev <coungdev@gmail.com>
2024-01-26 13:25:02 +0700

    Add VerifyEmailHdl endpoint with documentation

--- module/user/transport/verify_email_hdl.go
+++ module/user/transport/verify_email_hdl.go
@@ -7,6 +7,17 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// VerifyEmailHdl godoc
+// @Summary Verify email
+// @Description Verify email with code
+// @Tags user
+// @Accept application/json
+// @Produce application/json
+// @Param email query string true "Email"
+// @Param code query string true "Code"
+// @Success 200 {string} string "Success"
+// @Failure 500 {object} common.ErrResp "Internal Server Error"
+// @Router /users/verify-email [get]
 func (h httpHandler) VerifyEmailHdl() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		//get email and code from query string

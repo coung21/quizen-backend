bcaa3c10047be33525a28eb4caf800d0bd87c720
Coung <coungdev@gmail.com>
2024-02-08 13:28:28 +0700

    auth middleware

---
+++ common/const.go
@@ -0,0 +1,5 @@
+package common
+
+const (
+	CtxValUserIdKey = "user_id"
+)
--- module/user/transport/transport.go
+++ module/user/transport/transport.go
@@ -1,6 +1,8 @@
 package transport
 
 import (
+	"quizen/component/token"
+	"quizen/module/user/store"
 	"quizen/module/user/usecase"
 
 	"github.com/gin-gonic/gin"
@@ -14,7 +16,7 @@ func NewHTTPHandler(userUsecase usecase.Usecase) httpHandler {
 	return httpHandler{userUsecase: userUsecase}
 }
 
-func InitializeUserRoutes(hdl httpHandler, router *gin.RouterGroup) {
+func InitializeUserRoutes(hdl httpHandler, router *gin.RouterGroup, tokenprovider token.TokenProvider, store store.Store) {
 	router.POST("/register", hdl.CreateUserHandler())
 	router.GET("/verify-email", hdl.VerifyEmailHdl())
 	router.POST("/login", hdl.LoginHdl())

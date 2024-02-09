7c7f55552ca98f5ce935b40e00094a8cecbf618d
Coung <coungdev@gmail.com>
2024-02-09 23:32:21 +0700

    add delete set api doc

--- module/flashcard/transport/delete_study_set_hdl.go
+++ module/flashcard/transport/delete_study_set_hdl.go
@@ -7,6 +7,17 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// DeleteStudySetHdl godoc
+// @Summary Delete a study set
+// @Description Delete a study set
+// @Tags study-set
+// @Produce json
+// @Param id path string true "Study set ID"
+// @Param Authorization header string true "Bearer + Access Token"
+// @Success 204 {string} string "No content"
+// @Failure 404 {object} common.ErrResp "Study set not found"
+// @Failure 500 {object} common.ErrResp "Failed to delete study set"
+// @Router /study-set/{id} [delete]
 func (h httpHandler) DeleteStudySetHdl() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		studySetID := c.Param("id")
